2cbde9294e2d2c64a40f2b1cd2dae032e2cce7c8
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2023-08-23 20:50:10 +0200

    Attaching to meshes via sockets

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -18,6 +18,9 @@ void AShooterCharacter::BeginPlay()
 	Super::BeginPlay();
 	
 	Gun = GetWorld()->SpawnActor<AGun>(GunClass);
+	GetMesh()->HideBoneByName(TEXT("weapon_r"), EPhysBodyOp::PBO_None);
+	Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform, TEXT("WeaponSocket"));
+	Gun->SetOwner(this);
 }
 
 // Called every frame
