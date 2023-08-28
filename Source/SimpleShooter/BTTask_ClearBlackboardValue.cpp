f82f13050a838371740bfcb90df900ac5d8d8c4e
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2023-08-28 22:02:15 +0200

    Custom BTTasks in C++

---
+++ Source/SimpleShooter/BTTask_ClearBlackboardValue.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_ClearBlackboardValue.h"
+
+UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue()
+{
+	NodeName = TEXT("Clear Blackboard Value");
+}
---
+++ Source/SimpleShooter/BTTask_ClearBlackboardValue.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_ClearBlackboardValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API UBTTask_ClearBlackboardValue : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_ClearBlackboardValue();
+};
