6204bba9bc760bf7236b952a2d4abb4376006c7b
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2023-08-28 16:29:50 +0200

    Behavior Tree Tasks And Sequences

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -15,6 +15,7 @@ void AShooterAIController::BeginPlay()
 
 		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
 	}
 	else
 	{
